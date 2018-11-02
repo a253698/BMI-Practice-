c067ef019458bc31b3658db4c9aff3780ca7c1cd
Mavis <angel253698@gmail.com>
2018-11-02 23:57:06 +0800

    11/2 Toast

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +23,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void bmi(View view){
+        Log.d("MainActivity" , "bmi");
         findView();
         float weight = Float.parseFloat(edweight.getText().toString());
         float height = Float.parseFloat(edheight.getText().toString());
         float bmi = weight / (height * height);
-        Intent intent = new Intent(this,ResultActivity.class);
-        intent.putExtra("BMI" , bmi);
-        startActivity(intent);
+//        Intent intent = new Intent(this,ResultActivity.class);
+//        intent.putExtra("BMI" , bmi);
+//        startActivity(intent);
+
+        Toast.makeText(this, "Yout BMI is " + bmi , Toast.LENGTH_LONG).show();
+         new AlertDialog.Builder(this).setMessage("Your BMI is" + bmi)
+                 .setTitle("BMI").setPositiveButton("OK" , null)
+                 .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialog, int which) {
+                         edheight.setText(" ");
+                         edweight.setText(" ");
+                         //清除數字
+                     }
+                 }).show();
     }
 
     private void findView() {
@@ -38,8 +53,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("MainActivity" , "help");
+                // alt + enter .抽出
                  new AlertDialog.Builder(MainActivity.this).setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual.")
                          .setPositiveButton("ok" , null).show();
+
 
             }
         });
