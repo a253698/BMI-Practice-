c8fbbb6622124a461527187b035b0876dd06fc25
Mavis <angel253698@gmail.com>
2018-12-23 21:04:01 +0800

    restart

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private EditText edweight;
     private EditText edheight;
 
@@ -20,45 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edweight = findViewById(R.id.ed_weight);
+        edheight = findViewById(R.id.ed_height);
     }
 
     public void bmi(View view){
-        Log.d("MainActivity" , "bmi");
-        findView();
         float weight = Float.parseFloat(edweight.getText().toString());
         float height = Float.parseFloat(edheight.getText().toString());
-        float bmi = weight / (height * height);
-        Intent intent = new Intent(this,ResultActivity.class);
+        float bmi = weight / height * height ;
+
+        Intent intent = new Intent(this, ResultActivity.class);
         intent.putExtra("BMI" , bmi);
         startActivity(intent);
-
-        Toast.makeText(this, "Yout BMI is " + bmi , Toast.LENGTH_LONG).show();
-         new AlertDialog.Builder(this).setMessage("Your BMI is" + bmi)
-                 .setTitle("BMI").setPositiveButton("OK" , null)
-                 .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialog, int which) {
-                         edheight.setText(" ");
-                         edweight.setText(" ");
-                         //清除數字
-                     }
-                 }).show();
-    }
-
-    private void findView() {
-        edweight = findViewById(R.id.ed_weight);
-        edheight = findViewById(R.id.ed_height);
-        Button help = findViewById(R.id.help);
-        help.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("MainActivity" , "help");
-                // alt + enter .抽出
-                 new AlertDialog.Builder(MainActivity.this).setMessage(R.string.bmi_info)
-                         .setPositiveButton("ok" , null).show();
-
-
-            }
-        });
     }
 }
--- app/src/main/java/com/example/user/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/user/myapplication/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -11,9 +12,10 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        float bmi = getIntent().getFloatExtra("BMI" ,0);
-        TextView bmiresult = findViewById(R.id.bmiresult);
-        bmiresult.setText("Your BMI is " + bmi);
+        float bmi = getIntent().getFloatExtra("BMI" , 0);
+        TextView result = findViewById(R.id.bmiresult);
+        result.setText("Your BMI is " + bmi);
+
 
     }
 }
